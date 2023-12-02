650966192afe2ce2cff6099be3f68282279eb33a
AyaMouawi <ayamouawi8@gmail.com>
2023-12-03 00:15:11 +0200

    feat organizing

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,8 +1,7 @@
 const Cart = require('../models/cartModel');
 const Product = require('../models/productModel');
 const Order = require('../models/orderModel');
-const User = require('../models/userModel')
-const nodemailer = require('nodemailer');
+const {sendOrderNotificationEmailToOwner, sendOrderConfirmationEmailToClient} = require ('../extra/sendEmails')
 
 const createOrderFromCart = async (req, res) => {
     try {
@@ -15,6 +14,13 @@ const createOrderFromCart = async (req, res) => {
             return res.status(404).json({
                 success: false,
                 message: `No cart found with id ${cartID}`,
+            });
+        }
+
+        if (cart.productIds.length === 0) {
+            return res.status(400).json({
+                success: false,
+                message: 'Cannot checkout with an empty cart',
             });
         }
 
@@ -58,106 +64,7 @@ const createOrderFromCart = async (req, res) => {
     }
 };
 
-const sendOrderConfirmationEmailToClient = async (order) => {
-    try {
-        const transporter = nodemailer.createTransport({
-        
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASSWORD,
-            },
-        });
 
-     
-        const user = await User.findById(order.userId);
-
-        const productDetails = order.productIds.map(product => `
-            <div>
-                <p>Product ID: ${product._id}</p>
-                <p>Name: ${product.name}</p>
-                <img src="${product.images[0]}" alt="${product.name}" style="max-width: 100px;"/>
-                <p>Reference: ${product.reference}</p>
-                <p>Price: ${product.price}</p>
-            </div>
-        `).join('');
-
-        const mailOptions = {
-            from: process.env.EMAIL_USER,
-            to: user.email, 
-            subject: 'Order Confirmation',
-            html: `
-                <p>Thank you for your order!</p>
-                <p>Order ID: ${order._id}</p>
-                <p>User Details:</p>
-                <p>Name: ${user.fullName}</p>
-                <p>Email: ${user.email}</p>
-                <p>Address: ${user.address}</p>
-                <p>Phone Number: ${user.phoneNumber}</p>
-                <p>Product Details:</p>
-                ${productDetails}
-                <p>Total Price: ${order.totalPrice}</p>
-                <p>Shipping Method: ${order.shippingMethod}</p>
-                <p>Shipping Fee: ${order.shippingFee}</p>
-                <p>Payment Method: ${order.paymentMethod}</p>
-            `,
-        };
-
-        await transporter.sendMail(mailOptions);
-    } catch (error) {
-        console.error('Error sending email to client:', error);
-    }
-};
-
-const sendOrderNotificationEmailToOwner = async (order) => {
-    
-    try {
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASSWORD,
-            },
-        });
-
-        const user = await User.findById(order.userId);
-
-        const productDetails = order.productIds.map(product => `
-            <div>
-                <p>Product ID: ${product._id}</p>
-                <p>Name: ${product.name}</p>
-                <img src="${product.images[0]}" alt="${product.name}" style="max-width: 100px;"/>
-                <p>Reference: ${product.reference}</p>
-                <p>Price: ${product.price}</p>
-            </div>
-        `).join('');
-
-        const mailOptions = {
-            from: process.env.EMAIL_USER,
-            to: process.env.EMAIL_ADMIN, 
-            subject: 'New Order Notification',
-            html: `
-                <p>You have received a new order!</p>
-                <p>Order ID: ${order._id}</p>
-                <p>User Details:</p>
-                <p>Name: ${user.fullName}</p>
-                <p>Email: ${user.email}</p>
-                <p>Address: ${user.address}</p>
-                <p>Phone Number: ${user.phoneNumber}</p>
-                <p>Product Details:</p>
-                ${productDetails}
-                <p>Total Price: ${order.totalPrice}</p>
-                <p>Shipping Method: ${order.shippingMethod}</p>
-                <p>Shipping Fee: ${order.shippingFee}</p>
-                <p>Payment Method: ${order.paymentMethod}</p>
-            `,
-        };
-
-        await transporter.sendMail(mailOptions);
-    } catch (error) {
-        console.error('Error sending email to website owner:', error);
-    }
-};
 
 const deleteById = async (req, res) => {
     try {
